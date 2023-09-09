f40f18eff5379ff9b0fb30d9c7fc1d6f834166d1
Deyweson <devdeyve@gmail.com>
2023-09-09 17:55:14 -0300

    Uma melhor implementação, explicado melhor no arquivo implementação

--- checkbox/checkbox.js
+++ checkbox/checkbox.js
@@ -1,36 +1,70 @@
-const itensSelecionados = []
-const finalizarPedidoBotao = document.getElementById('finalizar-pedido') 
+//Obtenção dos objetos aonde serao adicionado os checkbox
+const manha = document.querySelector('.menu__cafe-da-manha')
+const divisoriaManha = manha.querySelectorAll('div p')
 
+const almoco = document.querySelector('.menu__almoco')
+const divisoriaAlmoco = almoco.querySelectorAll('div p')
 
-function finalizarPedido(){
-  const item = document.getElementsByName('item-cardapio')
+const jantar = document.querySelector('.menu__jantar')
+const divisoriaJantar = jantar.querySelectorAll('div p')
 
-  for(let i =0; i < item.length; i++){
-    if(item[i].checked){
-      itensSelecionados.push(item[i].value)
-    }
-  }
-
-  const filtroItensRepetidos = itensSelecionados.filter((a, b) => itensSelecionados.indexOf(a) === b)
-  console.log(filtroItensRepetidos)
-  console.log(itensSelecionados)
-
-  alert('Pedido Realizado')
+//Loop para adcionar em cada item na pagina, assim funcionando caso diminua ou aumente o numero de itens no cardapio e criação do objeto input(checbox) que sera adicionado dinamicamente na pagina
+for(let i = 0; i < divisoriaManha.length; i++){
+  const checkbox = document.createElement('input')
+  checkbox.type = 'checkbox'
+  divisoriaManha[i].append(checkbox)
+}
+for(let i = 0; i < divisoriaAlmoco.length; i++){
+  const checkbox = document.createElement('input')
+  checkbox.type = 'checkbox'
+  divisoriaAlmoco[i].append(checkbox)
+}
+for(let i = 0; i < divisoriaJantar.length; i++){
+  const checkbox = document.createElement('input')
+  checkbox.type = 'checkbox'
+  divisoriaJantar[i].append(checkbox)
 }
 
-const cardapio = document.querySelector('#cardapio')
+//Adição da funcionalidade do checkbox aonde adiciona a classe para mostrar vizualmente para o usuario que o item esta selecionado 
+const checkboxCafe = document.querySelector('.menu__cafe-da-manha')
+const checkboxAlmoco = document.querySelector('.menu__almoco')
+const checkboxJantar = document.querySelector('.menu__almoco')
 
-
-cardapio.addEventListener('change', (event) => {
+checkboxCafe.addEventListener('change', (event) => {
   const checkbox = event.target;
   const checked = checkbox.checked;
 
-  const div = checkbox.parentNode;
+  const div = checkbox.parentNode.parentNode;
 
   if(checked){
-    div.classList.add('teste')
+    div.classList.add('selecionado')
   }else{
-    div.classList.remove('teste')
+    div.classList.remove('selecionado')
   }
-})
+});
 
+checkboxAlmoco.addEventListener('change', (event) => {
+  const checkbox = event.target;
+  const checked = checkbox.checked;
+
+  const div = checkbox.parentNode.parentNode;
+
+  if(checked){
+    div.classList.add('selecionado')
+  }else{
+    div.classList.remove('selecionado')
+  }
+});
+
+checkboxJantar.addEventListener('change', (event) => {
+  const checkbox = event.target;
+  const checked = checkbox.checked;
+
+  const div = checkbox.parentNode.parentNode;
+
+  if(checked){
+    div.classList.add('selecionado')
+  }else{
+    div.classList.remove('selecionado')
+  }
+});
--- checkbox/implementacao.md
+++ checkbox/implementacao.md
@@ -1,3 +1,16 @@
+
 # Implementações realizadas
 
-- adicionado o arquivos para realizar a função do click no chebox aonde retorna o item selecionado, nao podendo ter varios itens identicos na lista, tambem realizando a alteração no css para representar visualmente a seleção do item do cardapio
+## Implementaçoes
+
+- O codigo se constitui em duas partes
+
+- A prmeira implementação do codigo adiciona o input do tipo checbox
+
+- A segunda implementaão do codigo adiciona uma borda em torno da imagem do item do cardapio selecionado  
+  
+
+## Observações
+
+
+- Entrei em contato com a pessoa responsável pelo front end para mudar a posição da linha divisória, aonde solicito para retira-la da div que fica os itens principais do cardápio como nome , imagem e preço, pois ao adicionar classe na imagem para representar que estava sendo selecionada, também estava adicionando na linha divisória aonde visivelmente não fica agradável, eu não consegui solucionar isso com código por isso eu fiz esse solicitação e também ira economizar linhas de código
